ce4c1bb093979b716e18a2d25e7b62df2e769058
SMImrulKabir <kabir.smimrul@gmail.com>
2021-07-06 23:44:44 -0400

    sprint3 changes

--- VoteCastUI/src/app/home/home.component.ts
+++ VoteCastUI/src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, Injector, ChangeDetectionStrategy } from '@angular/core';
 import { AppComponentBase } from '@shared/app-component-base';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
-import { UserLoginInfoDto } from '@shared/service-proxies/service-proxies';
 import { AppSessionService } from '@shared/session/app-session.service';
 
 @Component({
@@ -13,6 +12,9 @@ export class HomeComponent extends AppComponentBase {
   privacy_code: string = '';
   user: string;
   showBallot:boolean = false;
+  selectedCandidate:string = '';
+  voted:boolean = false;
+
   constructor(injector: Injector, private _userService: AppSessionService,
     ) {
     super(injector);
@@ -20,6 +22,43 @@ export class HomeComponent extends AppComponentBase {
   }
 
   getRandomNumber(){
-    this.privacy_code = '2130 F5T7 B890 7Y8U';
+    this.privacy_code = getRandomString(19);
   }
+
+  confirmVote(selection:string){
+    this.selectedCandidate = selection;
+    this.voted = true;
+  }
+
+  
+
+  //this data will come from API after API call is implemented
+  //Test data is for JavaScript code implementaion purpose only
+  testData = [
+    {
+      "name":"Donald Trump",
+      "pic": "assets/img/donald.jpg"
+    },
+    {
+      "name":"Barack Obama",
+      "pic": "assets/img/obama.jpg"
+    },
+    {
+      "name":"George Bush",
+      "pic": "assets/img/bush.jpg"
+    },
+    {
+      "name":"Bill Clinton",
+      "pic": "assets/img/clinton.jpg"
+    }
+  ];
+}
+
+function getRandomString(length) {
+  var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  var result = '';
+  for ( var i = 1; i <= length; i++ ) {
+      (i%5 == 0)? result += ' ':  result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return result;
 }
